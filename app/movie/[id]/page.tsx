c32f8efed7461285ba2159dbe9320b98fe39a084
Ceyhun1999 <rzayev-ceyhun-99@mail.ru>
2024-04-24 16:53:12 +0400

    04-24-2024

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -2,7 +2,12 @@ import MovieContainer from "@/containers/movieContainer/MovieContainer";
 import Movies from "@/mocks/movies.json";
 import { notFound } from "next/navigation";
 
-function MoviePage({ params }: { params: { id: string } }) {
+async function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function MoviePage({ params }: { params: { id: string } }) {
+    await delay(2000);
     const movieDetail = Movies.results.find((movie) => movie.id.toString() === params.id);
 
     if (!movieDetail) {
